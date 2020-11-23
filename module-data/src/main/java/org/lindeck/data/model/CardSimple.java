bebed21edf88c93165d413fd3ef1f0425905bce9
derovi <dirbraindrv@gmail.com>
2020-11-23 21:44:17 +0300

    CardSimple added

---
+++ module-data/src/main/java/org/lindeck/data/model/CardSimple.java
@@ -0,0 +1,13 @@
+package org.lindeck.data.model;
+
+import javax.persistence.Entity;
+
+/**
+ * @author derovi
+ * CardSimple has no fields, that extend AbstractCard,
+ * BUT it can change later!
+ */
+
+@Entity
+public class CardSimple extends AbstractCard {
+}
